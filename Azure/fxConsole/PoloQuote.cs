e6d606bf3fb7691e6fc33f5d73ebaebea68a4d4b
John Cote <postcreek@outlook.com>
2018-01-04 15:23:17 -0700

    initial commit

---
+++ Azure/fxConsole/PoloQuote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fxConsole
+{
+    public class PoloQuote
+    {
+        public int id { get; set; }
+        public decimal last { get; set; }
+        public decimal lowestAsk { get; set; }
+        public decimal highestBid { get; set; }
+        public decimal percentChange { get; set; }
+        public decimal baseVolume { get; set; }
+        public decimal quoteVolume { get; set; }
+        public decimal isFrozen { get; set; }
+        public decimal high24hr { get; set; }
+        public decimal low24hr { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("last:{0} ask:{1} bid:{2} chg:{3} high:{4} low:{5}", last, lowestAsk, highestBid, percentChange, high24hr, low24hr);
+        }
+    }
+}
---
+++ Azure/fxConsole/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace fxConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseBlob("1001_2.json");
+
+            Console.WriteLine("\nAny key to exit...");
+            Console.ReadKey();
+        }
+
+        static void ParseBlob(string name)
+        {
+            string prefix = name.Substring(0, 4);
+            string path = string.Format(@"c:\blobs\{0}\{1}", prefix, name);
+
+            Dictionary<string, PoloQuote> quotes = null;
+
+            using (StreamReader stream = File.OpenText(path))
+            {
+                string json = stream.ReadToEnd();
+                quotes = JsonConvert.DeserializeObject<Dictionary<string, PoloQuote>>(json);
+            }
+
+            if (quotes != null)
+            {
+                foreach (var key in quotes.Keys)
+                {
+                    Console.WriteLine("{0} {1}", key, quotes[key].ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Found nothing.");
+            }
+        }
+
+        static void DownloadBlobs(string prefix)
+        {
+            string connectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("datacollection");
+
+            List<string> names = new List<string>();
+            int ct = 1;
+
+            foreach (IListBlobItem item in container.ListBlobs(prefix, false))
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    names.Add(blob.Name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} | {1}", item.GetType().Name, item.Uri);
+                }
+
+                if (ct > 1000)
+                {
+                    Console.Write(".");
+                    ct = 0;
+                }
+
+                ct += 1;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Found {0} names", names.Count);
+            Console.WriteLine("\nAny key to continue...");
+            Console.ReadKey();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
+
+                using (var fileStream = System.IO.File.OpenWrite(string.Format(@"d:\{0}\{1}", prefix, name)))
+                {
+                    blockBlob.DownloadToStream(fileStream);
+                }
+            }
+        }
+    }
+}
